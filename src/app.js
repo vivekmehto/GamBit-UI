d2199d6965e6224b40aa0c0d992ef214367e6f0d
vivekmehto <vivekmehto.webdev@gmail.com>
2022-02-21 13:41:46 +0530

    Refactor: folder structure

--- src/app.js
+++ src/app.js
@@ -1,13 +1,10 @@
-const burger = document.querySelector(".burger");
+const burger = document.querySelector("#burger");
 const sidebarAside = document.querySelector("#sidebar-aside");
-const mainIntro = document.querySelector(".main-intro");
 
 burger.addEventListener("click", () => {
   if (sidebarAside.style.display === "none") {
     sidebarAside.style.display = "block";
-    mainIntro.style.marginLeft = "200px";
   } else {
     sidebarAside.style.display = "none";
-    mainIntro.style.marginLeft = "20px";
   }
 });
